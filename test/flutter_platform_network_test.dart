bd9c79cc258f33da24607bb64ef1660d004941a0
Artem Abramov <artem.abramov@dualbootpartners.com>
2019-10-12 15:03:50 +0400

    Added test for checking auth token not expired

--- test/flutter_platform_network_test.dart
+++ test/flutter_platform_network_test.dart
@@ -111,6 +111,30 @@ void main() {
 
     expect(response, isA<DioError>());
   });
+
+  test('Check auth token not expired', () async {
+    error = DioError(
+        request: RequestOptions(
+      path: "/",
+      extra: {AUTHORISED_REQUEST: true},
+    ));
+
+    when(delegate.isAuthorised()).thenAnswer((_) => Future.value(true));
+    when(delegate.isSameToken(error.request.headers))
+        .thenAnswer((_) => Future.value(true));
+    when(delegate.isAccessTokenExpired(error)).thenReturn(false);
+
+    final response = await interceptor.onError(error);
+
+    verify(delegate.isSameToken(error.request.headers)).called(1);
+    verify(delegate.isAccessTokenExpired(error)).called(1);
+
+    verifyNoMoreInteractions(delegate);
+    verifyNoMoreInteractions(apiDio);
+    verifyNoMoreInteractions(tokenDio);
+
+    expect(response, isA<DioError>());
+  });
 }
 
 class MockRefreshTokenInterceptorDelegate extends Mock
