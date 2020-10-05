f04dcf1ce1fa42dc01a036a8190c10cc481a7325
Artem Abramov <artem.abramov@dualbootpartners.com>
2020-10-05 16:43:52 +0300

    Fixed issue with unnecessary `onTokensRefreshingFailed` call

--- lib/src/api_client.dart
+++ lib/src/api_client.dart
@@ -186,7 +186,13 @@ abstract class ApiClient {
         if (error is DioError && delegate.isAccessTokenExpired(error)) {
           return _provider
               .getTokenManager()
-              .flatMap((tokenManager) => tokenManager.refreshTokens())
+              .flatMap((tokenManager) =>
+              tokenManager.refreshTokens().doOnError((refreshError) {
+                if (refreshError is DioError &&
+                    delegate.isRefreshTokenExpired(refreshError)) {
+                  delegate.onTokensRefreshingFailed();
+                }
+              }))
               .flatMap(performRequest);
         }
 
